fd15f9643873443c7ba3a95a40c2537ad0d2b504
zhopson <man24@yandex.ru>
2021-11-03 15:12:26 +0900

    first commit

---
+++ resources/js/router.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from './pages/Home.vue';
+import About from './pages/About.vue';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    linkExactActiveClass: 'active',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        },
+    ]
+});
+
+export default router;
+
